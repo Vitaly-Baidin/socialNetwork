721f07c77445c6c3dc9ded9d271fbe50681ea21d
Vitaly-Baidin <vitaliy.baidin.a@gmail.com>
2022-07-07 21:54:04 +0300

    added security

---
+++ src/main/java/com/viskei/social/models/Role.java
@@ -0,0 +1,34 @@
+package com.viskei.social.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Integer id;
+
+  @Column(length = 20)
+  private String name;
+
+  public Role() {
+
+  }
+
+  public Integer getId() {
+    return id;
+  }
+
+  public void setId(Integer id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+}
---
+++ src/main/java/com/viskei/social/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.viskei.social.repository;
+
+import com.viskei.social.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(String name);
+}
